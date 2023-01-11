4548243c7bc075395a33bb3ae0a887b44d12abf9
Inna Kotlovanovanova <Inna.Kotlovanova@gmail.com>
2023-01-11 21:49:19 +0100

    addind test

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,13 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-   Color initialColor =Colors.black12;
+  Color initialColor = Colors.black12;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,23 +20,24 @@ class _MyAppState extends State<MyApp> {
         body: InkWell(
           onTap: () {
             setState(() {
-              initialColor = Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
-                  .withOpacity(1.0);
+              initialColor =
+                  Color((math.Random().nextDouble() * 0xFFFFFF)
+                      .toInt(),).withOpacity(1.0);
             });
           },
           child: Container(
             width: double.infinity,
             height: double.infinity,
-            color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
-                .withOpacity(1.0),
+            color: Color((math.Random().nextDouble() * 0xFFFFFF)
+                .toInt(),).withOpacity(1.0),
             alignment: Alignment.center,
-            child: Text(
+            child: const Text(
               'Hey there',
               style: TextStyle(fontSize: 30, fontStyle: FontStyle.italic),
-              ),
             ),
+          ),
         ),
-        ),
+      ),
     );
   }
 }
